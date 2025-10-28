9e673ebfb32062dd823c1fe8f4b47320831da04c
6GOD <55304045+6ixGODD@users.noreply.github.com>
2025-10-28 12:58:51 +0800

    chore: generalize Orch class to support any PersonaX subtype

--- personax/orch.py
+++ personax/orch.py
@@ -4,15 +4,14 @@ import builtins
 import typing as t
 
 from personax import PersonaX
-from personax.utils import singleton
 
+T = t.TypeVar("T", bound=PersonaX)
 
-@singleton
-class Orch(t.MutableMapping[str, PersonaX]):
+class Orch(t.MutableMapping[str, T]):
     def __init__(self) -> None:
-        self._ins: dict[str, PersonaX] = {}
+        self._ins: dict[str, T] = {}
 
-    def register(self, persona: PersonaX) -> None:
+    def register(self, persona: T) -> None:
         if persona.id in self._ins:
             raise ValueError(f"Persona with id '{persona.id}' is already registered.")
         self._ins[persona.id] = persona
@@ -22,21 +21,21 @@ class Orch(t.MutableMapping[str, PersonaX]):
             raise KeyError(f"Persona with id '{persona_id}' is not registered.")
         del self._ins[persona_id]
 
-    def get(self, persona_id: str, /) -> PersonaX:
+    def get(self, persona_id: str, /) -> T:
         if persona_id not in self._ins:
             raise KeyError(f"Persona with id '{persona_id}' is not registered.")
         return self._ins[persona_id]
 
-    def list(self) -> builtins.list[PersonaX]:
+    def list(self) -> builtins.list[T]:
         return list(self._ins.values())
 
     def keys(self) -> t.KeysView[str]:
         return self._ins.keys()
 
-    def __getitem__(self, key: str, /) -> PersonaX:
+    def __getitem__(self, key: str, /) -> T:
         return self.get(key)
 
-    def __setitem__(self, key: str, /, value: PersonaX) -> None:
+    def __setitem__(self, key: str, /, value: T) -> None:
         if key != value.id:
             raise ValueError("Key must be the same as PersonaX.id")
         self.register(value)
